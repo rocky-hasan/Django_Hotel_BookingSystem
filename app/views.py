6ebf06518f2946712e6af279a7ceb0e86836e22f
rocky-hasan <rakibulhasancblock1a@gmail.com>
2024-01-10 19:49:39 +0600

    add some some code in Booking

--- app/views.py
+++ app/views.py
@@ -7,8 +7,24 @@ from django.db.models import Q
 def hotel(request):
     amenities_objs=Amenities.objects.all()
     hotels_objs=Hotel.objects.all()
+    sort_by=request.GET.get('sort_by')
+    search=request.GET.get('search')
+
+    if sort_by:
+        sort_by=request.GET.get('sort_by')
+        if sort_by =='ASC':
+            hotels_objs=hotels_objs.order_by('hotel_price')
+        elif sort_by =='DSC':
+            hotels_objs=hotels_objs.order_by('-hotel_price')
+    if search:
+        hotels_objs=hotels_objs.filter(
+            Q(hotel_name__icontains=search) |  Q(description__icontains=search))
+
+
     context={
         'amenities_objs':amenities_objs,
-        'hotels_objs':hotels_objs
+        'hotels_objs':hotels_objs,
+        'sort_by':sort_by,
+        'search':search,
     }
     return render(request, 'hotel.html',context)
